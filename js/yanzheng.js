5213e4ffd2fa9f44f56fb4eba125de9eeb315cc8
wangweiruning <397149747@qq.com>
2017-05-09 17:19:41 +0800

    修改
    修改后

--- js/yanzheng.js
+++ js/yanzheng.js
@@ -1,6 +1,64 @@
 window.onload=function(){
-// 使用者
-function isuseName(){
+
+  function SelectPullDown(box){
+    this.selectBox = document.getElementById(box);
+    this.selectIpt = document.getElementById(box).getElementsByTagName("input")[0];//传值表单
+    this.selectDivBtn = document.getElementById(box).getElementsByTagName("div")[0];//选中显示框
+    this.selectDivList = document.getElementById(box).getElementsByTagName("div")[1];////下拉框
+    this.selectSpan = document.getElementById(box).getElementsByTagName("span");//选项
+}
+SelectPullDown.prototype ={
+    selectBox : '',//document.getElementById('selectYear').getElementsByTagName("input")[0],
+    selectIpt : '',//document.getElementById('selectYear').getElementsByTagName("input")[0],
+    selectDivBtn : '',//document.getElementById('selectYear').getElementsByTagName("div")[0],
+    selectDivList : '',//document.getElementById(box).getElementsByTagName("div")[1];
+    selectSpan : '',//document.getElementById('selectYear').getElementsByTagName("span"),
+    iptEven : function(){
+        var self = this;
+        this.selectBox.onmouseover = function(){
+            self.selectListShow();
+        }    
+        this.selectBox.onmouseout = function(){
+            self.selectListHide();
+        }
+        return this;    
+    },
+    selectListShow: function(){
+        this.selectDivList.style.display = "block";
+    },
+    selectListHide: function(){
+        this.selectDivList.style.display = "none";
+    },
+    selectPick : function() {
+        var span = this.selectSpan,
+        self = this;
+        function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+        for(var i=0;i<span.length;i++){
+            span[i].onclick = function(){
+                self.selectDivBtn.innerHTML = trimStr(this.innerHTML);
+                self.selectIpt.value= trimStr(this.id);
+                self.selectListHide();
+                //chang事件触发
+                if (self.selectIpt.fireEvent) self.selectIpt.fireEvent('onchange');
+                else{ 
+                    var evt;
+                    evt = document.createEvent("HTMLEvents");  
+                    evt.initEvent("change", true, true);  
+                    self.selectIpt.dispatchEvent(evt);
+                };
+            }
+        }    
+        return this;
+    },
+    init : function(){
+        this.iptEven().selectPick();    
+    }
+};
+//调用方法
+var year = new SelectPullDown('selectYear');
+year.init();
+
+  function isuseName(){
     var obj=document.getElementById("useName");
     if(/^\W{2,}$/.test(obj.value)){
        obj.className="green_color";
@@ -69,7 +127,7 @@ function isstyle(){
 // 多行文本
 function moreTst(){
     var obj=document.getElementById("moreText");
-    if(!obj.value==""){
+    if(obj.value != ""){
        return true;
     }else{
        return false;
